2e3331df40cf8a6dd17d8f2ae63d260aacdf413c
edamiyan <ed.amiyan@ya.ru>
2021-07-19 01:10:06 +0300

    Done base project struct

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"github.com/edamiyan/todo-app"
+	"github.com/edamiyan/todo-app/pkg/handler"
+	"github.com/edamiyan/todo-app/pkg/repository"
+	"github.com/edamiyan/todo-app/pkg/service"
 	"log"
 )
 
 func main()  {
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 
 	}
