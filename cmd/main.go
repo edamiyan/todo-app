f6178f35dcd37ecef4a263918f2859dfeb880e2e
edamiyan <ed.amiyan@ya.ru>
2021-08-14 00:53:40 +0300

    Add Graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"github.com/edamiyan/todo-app"
 	"github.com/edamiyan/todo-app/pkg/handler"
 	"github.com/edamiyan/todo-app/pkg/repository"
@@ -10,6 +11,8 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -40,9 +43,24 @@ func main() {
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
-		logrus.Fatalf("error occured while running http server: %s", err.Error())
+	go func() {
+		if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+			logrus.Fatalf("error occured while running http server: %s", err.Error())
+		}
+	}()
+	logrus.Print("Todo App started")
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+	logrus.Print("Todo App Shutting down")
+
+	if err = srv.Shutdown(context.Background()); err != nil {
+		logrus.Error("Error occured on server shutting down: %s", err.Error())
 	}
+	if err = db.Close(); err != nil {
+		logrus.Error("Error occured on connection db close: %s", err.Error())
+	}
+
 }
 
 func initConfig() error {
--- pkg/repository/todo_list_postgres.go
+++ pkg/repository/todo_list_postgres.go
@@ -24,14 +24,19 @@ func (r *TodoListPostgres) Create(userId int, list todo.TodoList) (int, error) {
 	createListQuery := fmt.Sprintf("INSERT INTO %s (title, description) VALUES ($1, $2) RETURNING id", todoListsTable)
 	row := tx.QueryRow(createListQuery, list.Title, list.Description)
 	if err := row.Scan(&id); err != nil {
-		_ = tx.Rollback()
+		if err = tx.Rollback(); err != nil {
+			return 0, err
+		}
 		return 0, err
 	}
 
 	createUsersListQuery := fmt.Sprintf("INSERT INTO %s (user_id, list_id) VALUES ($1, $2)", usersListsTable)
 	_, err = tx.Exec(createUsersListQuery, userId, id)
 	if err != nil {
-		_ = tx.Rollback()
+		if err := tx.Rollback(); err != nil {
+			return 0, err
+		}
+		return 0, err
 	}
 
 	return id, tx.Commit()
