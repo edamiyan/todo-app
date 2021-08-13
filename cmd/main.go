eb6f942cd55859b9b71c7b24f961518d1216a031
edamiyan <ed.amiyan@ya.ru>
2021-08-14 00:59:18 +0300

    Fix shutdown error msg

--- cmd/main.go
+++ cmd/main.go
@@ -44,7 +44,7 @@ func main() {
 
 	srv := new(todo.Server)
 	go func() {
-		if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+		if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil && err.Error() != "http: Server closed" {
 			logrus.Fatalf("error occured while running http server: %s", err.Error())
 		}
 	}()
